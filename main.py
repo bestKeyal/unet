fc6a4eae4841d871d82f95f4071545ccb08ba680
Keyal <1920194156@qq.com>
2024-04-20 18:04:24 +0800

    s

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ data_gen_args = dict(
 )
 
 num_CV = 1  # 这里是交叉验证的折数
-NumEpochs = 300  # 这里控制训练的epoch数量
+NumEpochs = 100  # 这里控制训练的epoch数量
 NumEpochEval = 0  # validated the model each NumEpochEval epochs
 batch_size = 50  # batch_size的设置
 learning_rateI = 1e-5
