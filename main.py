830f9d90efd3a436456c1bbb8201f1774ab70381
Keyal <1920194156@qq.com>
2024-04-20 16:42:16 +0800

    s

--- main.py
+++ main.py
@@ -271,8 +271,9 @@ if __name__ == '__main__':
             else:
                 subjectNums_cvI_testing = subject_nums_shaffled[cvI * int(numSubj / num_CV):numSubj]
         else:
-            subjectNums_cvI_testing = [80, 27, 21]
-            subjectNums_cvI_trainVal = [x for x in subject_nums_shaffled if x not in [80, 27, 21]]
+            subjectNums_cvI_testing = [80, 22]
+            subjectNums_cvI_trainVal = [80, 22]
+            # subjectNums_cvI_trainVal = [x for x in subject_nums_shaffled if x not in [80, 27]]
             # Finding the predictions or ICH segmentation for the whole slice
         print(
             'Combining the crops masks to find the full CT mask after performing morphological operations and saving the results to: ' + str(
